d5febdbcd8a654a057a75a0046674eb924d85a7e
tatsuya828828 <kkjy011894@gmail.com>
2020-07-19 23:32:41 +0900

    検索結果を画面に返す処理を追加

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.trySpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 // @ControllerアノテーションをつけることでDI(依存性注入)で利用できるようになる
 @Controller
 public class HelloController {
+	@Autowired
+	private HelloService helloService;
 	// GetMappingアノテーションをメソッドにつけるとHTTPリクエストのGETメソッドを処理できるようになる
 	// GETリクエストに対する処理をgetHelloメソッドで行うという意味になる
 	// なお、GETリクエストの場合、メソッド名の最初にgetをつけるのが慣習となっている
@@ -28,5 +31,23 @@ public class HelloController {
 		model.addAttribute("sample", str);
 		// helloResponse.htmlに画面遷移
 		return "helloResponse";
+	}
+
+	// @RequestParamアノテーションでtext2という名前のパラメーターを受け取る
+	// あとはその値をHelloServiceクラスのfindOneメソッドに渡せば、検索結果としてEmployeeのインスタンスが返ってくる
+	// また、employeeクラスの値をmodel.addAttributeで登録しておけば、画面で受け取ることができる
+	@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2")String str, Model model) {
+		// Stringからint型に変換
+		int id = Integer.parseInt(str);
+		// 1件検索
+		Employee employee = helloService.findOne(id);
+
+		// 検索結果をModelに登録
+		model.addAttribute("id", employee.getEmployeeId());
+		model.addAttribute("name", employee.getEmployeeName());
+		model.addAttribute("age", employee.getAge());
+		// helloResponsiveDB.htmlに画面遷移
+		return "helloResponseDB";
 	}
 }
