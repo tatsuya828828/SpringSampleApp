c89b8e3a8ccc862f80a433faafea77f69593894b
tatsuya828828 <kkjy011894@gmail.com>
2020-07-20 21:28:57 +0900

    loginControllerの作成

--- src/main/java/com/example/demo/login/controller/LoginController.java
+++ src/main/java/com/example/demo/login/controller/LoginController.java
@@ -1,5 +1,24 @@
 package com.example.demo.login.controller;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
+public class LoginController {
+	// ログイン画面のGET用コントローラー
+	@GetMapping("/login")
+	public String getLogin(Model model) {
+		// login.htmlに画面遷移
+		// login/loginというのは、loginフォルダ配下のlogin.htmlを指定している
+		return "login/login";
+	}
+
+	// ログイン画面のPOST用のコントローラー
+	@PostMapping("/login")
+	public String postLogin(Model model) {
+		// login.htmlに画面遷移
+		return "login/login";
+	}
 }
