28b0226aff971947e2e021477b51325eb576d74c
tatsuya828828 <kkjy011894@gmail.com>
2020-07-26 00:06:28 +0900

    ユーザーテーブルの作成

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,6 @@
 -- 従業員テーブル
 -- 従業員ID, 従業員名, 年齢
 CREATE TABLE IF NOT EXISTS employee(employee_id INT PRIMARY KEY, employee_name VARCHAR(50), age INT);
+
+-- ユーザーマスター
+CREATE TABLE IF NOT EXISTS m_user(user_id VARCHAR(50) PRIMARY KEY, password VARCHAR(100), user_name VARCHAR(50), birthday DATE, age INT, marriage BOOLEAN, role VARCHAR(50));
