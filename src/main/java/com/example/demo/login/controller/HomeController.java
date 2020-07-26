de5c8a74ff6252d5feaee34164634c6dde8e080e
tatsuya828828 <kkjy011894@gmail.com>
2020-07-26 22:32:02 +0900

    データベースから全ユーザーの情報を取ってくるように設定

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.example.demo.login.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.service.UserService;
 
 @Controller
@@ -24,6 +27,22 @@ public class HomeController {
 		return "login/homeLayout";
 	}
 
+	// ユーザー一覧画面のGET用メソッド
+	@GetMapping("/userList")
+	public String getUserList(Model model) {
+		// コンテンツ部分にユーザー一覧を表示するための文字列を登録
+		model.addAttribute("contents", "login/userList :: userList_contents");
+		// ユーザー一覧の生成
+		List<User> userList = userService.selectMany();
+		// Modelにユーザーリストを登録
+		model.addAttribute("userList", userList);
+		// データ件数を取得
+		int count = userService.count();
+		model.addAttribute("userListCount", count);
+
+		return "login/homeLayout";
+	}
+
 	// ログアウト用メソッド
 	@PostMapping("/logout")
 	public String postLogout() {
@@ -31,5 +50,10 @@ public class HomeController {
 		return "redirect:/login";
 	}
 
-
+	// ユーザー一覧のCSV出力用メソッド
+	@GetMapping("/userList/csv")
+	public String getUserListCsv(Model model) {
+		// 現段階では、何もせずにユーザー一覧画面へ戻す
+		return getUserList(model);
+	}
 }
