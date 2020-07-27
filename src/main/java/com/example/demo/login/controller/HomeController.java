da364b401624abb654a6e8d5e174a9218132d3da
tatsuya828828 <kkjy011894@gmail.com>
2020-07-27 11:10:12 +0900

    ユーザー詳細画面用のGETメソッドを追加

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.example.demo.login.controller;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.SignupForm;
 import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.service.UserService;
 
@@ -15,6 +20,18 @@ import com.example.demo.login.domain.service.UserService;
 public class HomeController {
 	@Autowired
 	UserService userService;
+
+	// 結婚ステータスのラジオボタン用変数
+	private Map<String, String> radioMarriage;
+	// ラジオボタンの初期化メソッド
+	private Map<String, String> initRadioMarriage() {
+		Map<String, String> radio = new LinkedHashMap<>();
+		// 既婚、未婚をMapに格納
+		radio.put("既婚", "true");
+		radio.put("未婚", "false");
+
+		return radio;
+	}
 
 	// ホーム画面のGET用メソッド
 	// /homeにGETリクエストが来たとき、Modelクラスのcontentsというキーにlogin/home :: home_contentsという値をセットしている
@@ -40,6 +57,41 @@ public class HomeController {
 		int count = userService.count();
 		model.addAttribute("userListCount", count);
 
+		return "login/homeLayout";
+	}
+
+	// 動的URL
+	/* 動的URLに対応したメソッドを作るためには、@GetMappingや@PostMappingの値に/{変数名}をつける
+	 * 例えば、ユーザーIDを受け取る場合は、@GetMapping(/userDetail/{id})とする
+	 * 今回の場合は、ユーザーIDがメールアドレス形式となっているため上のような書き方では処理がうまくいかない
+	 * それに対応するため今回は正規表現を使い{id:.+}となっている
+	 */
+	// ユーザー詳細画面のGET用メソッド
+	@GetMapping("/userDetail/{id:.+}")
+	// @PathVariableアノテーションをつけると、渡されてきたパス(URL)の値を引数の変数に入れることができる
+	// 今回の場合でいうとURLの{id:.+}の部分の値が引数のuserIdという変数に入れられる
+	public String getUserDetail(@ModelAttribute SignupForm form, Model model, @PathVariable("id") String userId) {
+		// ユーザーID確認(デバッグ)
+		System.out.println("userId ="+ userId);
+		// コンテンツ部分にユーザー詳細を表示するための文字列を登録
+		model.addAttribute("contents", "login/userDetail :: userDetail_contents");
+		// 結婚ステータスようラジオボタンの初期化
+		radioMarriage = initRadioMarriage();
+		// ラジオボタン用のMapをModelに登録
+		model.addAttribute("radioMarriage", radioMarriage);
+		// ユーザーIDのチェック
+		if(userId != null && userId.length() > 0) {
+			// ユーザー情報を取得
+			User user = userService.selectOne(userId);
+			// Userクラスをフォームクラスに変換
+			form.setUserId(user.getUserId());
+			form.setUserName(user.getUserName());
+			form.setBirthday(user.getBirthday());
+			form.setAge(user.getAge());
+			form.setMarriage(user.isMarriage());
+			// Modelに登録
+			model.addAttribute("signupForm", form);
+		}
 		return "login/homeLayout";
 	}
 
