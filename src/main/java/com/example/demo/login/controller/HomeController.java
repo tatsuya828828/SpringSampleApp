dea428eb23e4a5f08a7c82dfcd765778cedbc736
tatsuya828828 <kkjy011894@gmail.com>
2020-07-28 21:55:21 +0900

    HomeControllerにupdateメソッドを追加

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -95,6 +95,33 @@ public class HomeController {
 		return "login/homeLayout";
 	}
 
+	// ボタン名によるメソッド判定
+	// ユーザー更新用処理
+	// 更新処理用のメソッドと削除用のメソッドを分けるためにparams属性を使い同じURLのPOSTでもボタン名で判別できるようにする
+	@PostMapping(value = "/userDetail", params="update")
+	public String postUserDetailUpdate(@ModelAttribute SignupForm form, Model model) {
+		System.out.println("更新ボタンの処理");
+		// Userインスタンスの生成
+		User user = new User();
+		// フォームクラスをUserクラスに変換
+		user.setUserId(form.getUserId());
+		user.setPassword(form.getPassword());
+		user.setUserName(form.getUserName());
+		user.setBirthday(form.getBirthday());
+		user.setAge(form.getAge());
+		user.setMarriage(form.isMarriage());
+
+		// 更新実行
+		boolean result = userService.updateOne(user);
+		if(result == true) {
+			model.addAttribute("result", "更新成功");
+		} else {
+			model.addAttribute("result", "更新失敗");
+		}
+		// ユーザー一覧画面を表示
+		return getUserList(model);
+	}
+
 	// ログアウト用メソッド
 	@PostMapping("/logout")
 	public String postLogout() {
