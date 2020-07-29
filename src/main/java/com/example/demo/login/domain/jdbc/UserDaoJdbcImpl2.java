a5e044247d264585163a35b6a834bbca0283f8c4
tatsuya828828 <kkjy011894@gmail.com>
2020-07-29 18:37:31 +0900

    UserDaoJdbcImpl2クラスを作成

---
+++ src/main/java/com/example/demo/login/domain/jdbc/UserDaoJdbcImpl2.java
@@ -0,0 +1,38 @@
+package com.example.demo.login.domain.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.login.domain.model.User;
+
+@Repository("UserDaoJdbcImpl2")
+public class UserDaoJdbcImpl2 extends UserDaoJdbcImpl {
+	@Autowired
+	private JdbcTemplate jdbc;
+	// ユーザー1件取得
+	@Override
+	public User selectOne(String userId) {
+		// 1件取得用SQL
+		String sql = "SELECT * FROM m_user WHERE user_id=?";
+		// RowMapperの生成
+		UserRowMapper rowMapper = new UserRowMapper();
+		// SQL実行
+		return jdbc.queryForObject(sql, rowMapper, userId);
+	}
+
+	// ユーザー全権取得
+	@Override
+	public List<User> selectMany() {
+		// M_USERテーブルのデータを全件取得する SQL
+		String sql = "SELECT * FROM m_user";
+		// RowMapperの生成
+		RowMapper<User> rowMapper = new UserRowMapper();
+		// SQL実行
+		// JdbcTemplateのメソッドの引数に、RowMapperをセットすることでselect結果が入ったUserクラスやList<User>の結果が返ってくる
+		return jdbc.query(sql, rowMapper);
+	}
+}
